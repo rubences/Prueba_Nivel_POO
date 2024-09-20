9d45922cee3f97f964fcc47cef931f2560a99b58
rubences <91731294+rubences@users.noreply.github.com>
2024-09-20 17:10:01 +0000

    mas cambios

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from subsubclases.motocicleta import Motocicleta
 if __name__ == "__main__":
     mi_vehiculo = Coche("Rojo", 4, 180, 2000)
     print(mi_vehiculo)
-    mi_vehiculo2 = Bicicleta("Azul", 4, 150, 1200)
-    print (mi_vehiculo2)
+    mi_vehiculo2 = Bicicleta("Azul", "Montaña", "Azul", "Deportiva")
+    print(mi_vehiculo2)
     mi_vehiculo3 = Camioneta("Ford", "Fiesta", 1000)
     print (mi_vehiculo3)
     mi_vehiculo4 = Motocicleta("Negra", 2, 220, 1000)
